846f3a39a5d964389a0f2b66b4e953824c03d356
MattyVermulst <>
2023-09-09 21:47:37 +0000

    add scores and variables

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let playerScore = 0;
+let computerScore = 0;
+const rounds = 3;
+const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+const game = document.querySelector('.game');
+const buttons = [...document.querySelectorAll('.options button')];
+const scoreboard = document.getElementById('combined-scores');
+const outcome = document.getElementById('outcome');
+const playerResult = document.getElementById('player-result');
+const computerResult = document.getElementById('computer-result');
+const reset = document.getElementById('reset');
